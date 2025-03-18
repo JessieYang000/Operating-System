47d8d537b03b61f6cdec2440f9fccb0d9867700d
JessieYang000 <yang.jieq@northeastern.edu>
2025-03-18 10:07:28 -0400

    declare functions in header file

--- Practicum1/message.h
+++ Practicum1/message.h
@@ -12,4 +12,16 @@ typedef struct {
     int delivered;         // Delivery status (0 = Not delivered, 1 = Delivered)
 } Message;
 
+// Creates a new message and initializes its fields
+Message* create_msg(int id, const char* sender, const char* receiver, const char* content);
+
+//Stores a message persistently in a file
+int store_msg(const Message* msg, const char* filename);
+
+//Retrieves a message from a file based on its unique identifier
+Message* retrieve_msg(int id, const char* filename);
+
+//Frees the memory allocated for a Message struct
+void free_msg(Message* msg); 
+
 #endif
