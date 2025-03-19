0e65b47c83ee339b0bc9ea9e424336e4070fd2ac
JessieYang000 <yang.jieq@northeastern.edu>
2025-03-19 11:04:40 -0400

    add declarations for cache mechanism in header file

--- Practicum1/message.h
+++ Practicum1/message.h
@@ -3,6 +3,10 @@
 #define MESSAGE_H
 
 #define MESSAGE_DIR "messages/" // Directory to store message files
+#define CACHE_SIZE 16  // Number of messages in cache
+#define MESSAGE_SIZE 1024  // Fixed message size in bytes
+
+#include "uthash.h"
 
 // Define the Message struct
 typedef struct {
@@ -12,8 +16,14 @@ typedef struct {
     char receiver[50];     // Receiver's name
     char content[256];     // Message content
     int delivered;         // Delivery status (0 = Not delivered, 1 = Delivered)
+    struct Message* prev;  // For doubly linked list (LRU)
+    struct Message* next;  // For doubly linked list (LRU)
+    UT_hash_handle hh;     // UTHash handle for fast lookup
 } Message;
 
+/**
+ * Function declarations related to Message structure
+ */
 // Creates a new message and initializes its fields
 Message* create_msg(int id, const char* sender, const char* receiver, const char* content);
 
@@ -25,4 +35,17 @@ Message* retrieve_msg(int id);
 
 //Delete a message by ID
 void delete_msg(int id);
+
+// Free the memory allocation for a message
+void free_msg(Message* msg);
+
+/**
+ * Function declarations related to cache operations
+ */
+
+void init_cache();
+Message* cache_lookup(int id);
+void cache_insert(Message* msg);
+void cache_remove(int id);
+void cache_free();
 #endif
