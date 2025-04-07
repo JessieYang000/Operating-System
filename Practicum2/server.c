db8081056ef8d5e81e84c4e9380edfe7df241869
JessieYang000 <yang.jieq@northeastern.edu>
2025-04-07 09:55:44 -0400

    update to accept multiple clients with fork()

--- Practicum2/server.c
+++ Practicum2/server.c
@@ -1,7 +1,7 @@
 /*
  * server.c -- TCP Socket Server
- * 
- * adapted from: 
+ *
+ * adapted from:
  *   https://www.educative.io/answers/how-to-implement-tcp-sockets-in-c
  */
 
@@ -10,6 +10,8 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdlib.h>    // for exit()
+#include <sys/types.h> // for pid_t
 
 int main(void)
 {
@@ -17,79 +19,107 @@ int main(void)
   socklen_t client_size;
   struct sockaddr_in server_addr, client_addr;
   char server_message[8196], client_message[8196];
-  
+
   // Create socket:
   socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-  
-  if(socket_desc < 0){
+
+  if (socket_desc < 0)
+  {
     printf("Error while creating socket\n");
     return -1;
   }
   printf("Socket created successfully\n");
-  
+
   // Set port and IP:
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(2000);
   server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  
+
   // Bind to the set port and IP:
-  if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr))<0){
+  if (bind(socket_desc, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+  {
     printf("Couldn't bind to the port\n");
     return -1;
   }
   printf("Done with binding\n");
-  
+
   // Listen for clients:
-  if(listen(socket_desc, 1) < 0){
+  if (listen(socket_desc, 5) < 0)
+  { // Allow up to 5 pending clients
     printf("Error while listening\n");
     close(socket_desc);
     return -1;
   }
   printf("\nListening for incoming connections.....\n");
-  
-  // Accept an incoming connection:
-  client_size = sizeof(client_addr);
-  client_sock = accept(socket_desc, (struct sockaddr*)&client_addr, &client_size);
-  
-  if (client_sock < 0){
-    printf("Can't accept\n");
-    close(socket_desc);
-    close(client_sock);
-    return -1;
-  }
-  printf("Client connected at IP: %s and port: %i\n", 
-         inet_ntoa(client_addr.sin_addr), 
-         ntohs(client_addr.sin_port));
-  
-  // Loop to handle multiple messages from the same client
-  while (1) {
-    memset(client_message, '\0', sizeof(client_message));
-    memset(server_message, '\0', sizeof(server_message));
 
-    // Receive client's message:
-    if (recv(client_sock, client_message, sizeof(client_message), 0) < 0){
-      printf("Couldn't receive\n");
-      break;
+  // loop for accepting multiple clients
+  while (1)
+  {
+    // Accept an incoming connection:
+    client_size = sizeof(client_addr);
+    client_sock = accept(socket_desc, (struct sockaddr *)&client_addr, &client_size);
+
+    if (client_sock < 0)
+    {
+      printf("Can't accept\n");
+      continue;
     }
+    printf("Client connected at IP: %s and port: %i\n",
+           inet_ntoa(client_addr.sin_addr),
+           ntohs(client_addr.sin_port));
 
-    printf("Msg from client: %s\n", client_message);
+    // Fork a process to handle this client
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+      // CHILD process
+      close(socket_desc); // Child doesn't need the listener
 
-    if (strcmp(client_message, "exit") == 0){ 
-      printf("Client requested to close the connection.\n");
-      break;
+      while (1)
+      {
+        memset(client_message, '\0', sizeof(client_message));
+        memset(server_message, '\0', sizeof(server_message));
+
+        // Receive client's message:
+        if (recv(client_sock, client_message, sizeof(client_message), 0) <= 0) // Connection closed by client or error occurred
+
+        {
+          printf("Couldn't receive\n");
+          break;
+        }
+
+        printf("Msg from client: %s\n", client_message);
+
+        if (strcmp(client_message, "exit") == 0)
+        {
+          printf("Client requested to close the connection.\n");
+          break;
+        }
+
+        // Respond to client:
+        sprintf(server_message, "Server received: %s", client_message); // writes the formatted string to a buffer (server_message), so it is a dynamic response
+
+        if (send(client_sock, server_message, strlen(server_message), 0) < 0)
+        {
+          printf("Can't send\n");
+          break;
+        }
+      }
+      close(client_sock);
+      exit(0); // Exit child process
     }
-
-    // Respond to client:
-    sprintf(server_message, "Server received: %s", client_message);  //writes the formatted string to a buffer (server_message), so it is a dynamic response
-
-    if (send(client_sock, server_message, strlen(server_message), 0) < 0){
-      printf("Can't send\n");
-      break;
+    else if (pid > 0)
+    {
+      // PARENT process
+      close(client_sock); // Parent doesn't handle this client
+    }
+    else
+    {
+      perror("fork failed");
     }
   }
 
   // Closing the socket:
-  close(client_sock);
   close(socket_desc);
   return 0;
 }
